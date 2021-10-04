eb1e84b90a8f1fdcfd46334057fe1682ff63cf0a
Yaroslav Sedyukov <sedyukovofficial@gmail.com>
2021-10-04 12:29:46 +0300

    Добавьте файлы проекта.

---
+++ KgPractice2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace KgPractice2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw Koh snowflake";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Resize += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Koh snowflake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ KgPractice2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KgPractice2
+{
+    public partial class Form1 : Form
+    {
+        static Graphics holst;
+        static Pen pen_green;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int FractalKoh(PointF p1, PointF p2, PointF p3, int iter)
+        {
+            if (iter > 0)
+            {
+                var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
+                var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+
+                var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
+                var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
+
+                holst.DrawLine(pen_green, p4, pn);
+                holst.DrawLine(pen_green, p5, pn);
+                holst.DrawLine(pen_green, p4, p5);
+
+                FractalKoh(p4, pn, p5, iter - 1);
+                FractalKoh(pn, p5, p4, iter - 1);
+                FractalKoh(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iter - 1);
+                FractalKoh(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iter - 1);
+            }
+            return iter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            pen_green = new Pen(Color.Green, 3);
+            holst = CreateGraphics();
+            holst.Clear(Color.White);
+
+            var point1 = new PointF(ClientSize.Width / 3, ClientSize.Height / 3);
+            var point2 = new PointF((ClientSize.Width / 3) * 2, ClientSize.Height / 3);
+            var point3 = new PointF(ClientSize.Width / 2, (ClientSize.Height / 3) * 2);
+
+            holst.DrawLine(pen_green, point1, point2);
+            holst.DrawLine(pen_green, point2, point3);
+            holst.DrawLine(pen_green, point3, point1);
+
+            FractalKoh(point1, point2, point3, 4);
+            FractalKoh(point2, point3, point1, 4);
+            FractalKoh(point3, point1, point2, 4);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
